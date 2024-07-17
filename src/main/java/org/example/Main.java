6cb6cfe119f8c773604c655909d5013ad6c71941
meownly <175831477+meownly@users.noreply.github.com>
2024-07-17 20:04:23 +0530

    getting there.....

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,6 @@ class Main {
     public static void main(String[] args) {
         lc_560 ob = new lc_560();
         int[] nums = {1,2,3};
-        int x = 0;//ss
         int k = 3;
         System.out.println(ob.subarraySum(nums,k));
     }
