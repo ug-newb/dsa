031916935555cbb366586dc7f3edea7383d00058
meownly <117140701+ug-newb@users.noreply.github.com>
2024-07-28 18:45:28 +0530

    Linked list revisited

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,6 @@
 package org.example;
-import org.leetcode.lc_1;
 class Main {
     public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
-        int target = 9;
-        System.out.println("hello");
-        lc_1 ob = new lc_1();
-        int[] res = ob.twoSum(nums, target);
-        for(int i=0; i< res.length; i++) {
-            System.out.print(res[i] +" ");
-        }
+
     }
 }
