b3e3aefac323efcb2f78d8f458ada7deebc06f92
ug-newb <117140701+ug-newb@users.noreply.github.com>
2024-07-30 12:36:04 +0530

    Revert "solved some"
    This reverts commit 5c4650e99f3a4c473ab56564b8fba6ae4a3f3390.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
+import org.leetcode.lc_2461;
 class Main {
     public static void main(String[] args) {
-
+        int[] nums = {1,5,4,2,9,9,9};
+        int k = 3;
+        lc_2461 ob = new lc_2461();
+        System.out.println(ob.maximumSubarraySum(nums, k));
     }
 }
