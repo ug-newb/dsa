3cb12a053f61915f8d781d7618cdf9ca9f406d34
meownly <117140701+ug-newb@users.noreply.github.com>
2024-08-06 23:52:20 +0530

    TreeNode defintion

---
+++ src/main/java/org/definition/TreeNode.java
@@ -0,0 +1,16 @@
+package org.definition;
+
+public class TreeNode {
+    public int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) {
+        this.val =  val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
 import org.generalPractice.sortingAlgos;
+/*
+        *****
+*/
 class Main {
     public static void main(String[] args) {
-        sortingAlgos ob = new sortingAlgos();
-        int[] nums = {3,45,53,1,5,6,2, 7,};
-        ob.bubbleSort(nums);
+
     }
 }
